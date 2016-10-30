f8ec35d886aa9e4e61349332332cdaaa081b6dff
Matthew Chan <matthew.tk.chan@gmail.com>
2016-10-30 01:16:38 -0700

    applied CNN to the MNIST problem in CNTK. Not really working very well with an error rate of 0.18.

--- MNIST/minist_softmax_cntk.py
+++ MNIST/minist_softmax_cntk.py
@@ -2,8 +2,9 @@
 import gzip
 import os
 import struct
+import numpy as np
 import cntk
-from cntk.ops import *
+from cntk.layers import Convolution, MaxPooling, AveragePooling, Dropout, BatchNormalization, Dense
 
 try:
     from urllib.request import urlretrieve
@@ -152,84 +153,97 @@ labels_test = test_mb_source[labels_stream_name]
 print("Test data from file {0} successfully read".format(testing_data_path))
 
 
-num_hidden_layers = 2
-hidden_layers_dim = 400
 
-input = input_variable(input_dim, np.float32)
-label = input_variable(output_dim, np.float32)
+image_shape = (1, 28, 28)
+input = cntk.ops.input_variable(image_shape, np.float32)
+label = cntk.ops.input_variable(output_dim, np.float32)
 
-# Define a fully connected feedforward network
-def linear_layer(input_var, output_dim):
 
-    input_dim = input_var.shape[0]
-    times_param = parameter(shape=(input_dim, output_dim), init=cntk.initializer.glorot_uniform())
-    bias_param = parameter(shape=(output_dim))
+def create_basic_model(input, out_dims, dropout=0.0):
+    net = Convolution((5, 5), 32, init=cntk.initializer.glorot_uniform(scale=0.1557 / 256), activation=cntk.ops.relu, pad=True)(input)
+    net = MaxPooling((3, 3), strides=(2, 2))(net)
 
-    t = times(input_var, times_param)
-    return bias_param + t
+    net = Convolution((5, 5), 32, init=cntk.initializer.glorot_uniform(scale=0.2), activation=cntk.ops.relu, pad=True)(net)
+    net = MaxPooling((3, 3), strides=(2, 2))(net)
 
-def dense_layer(input, output_dim, nonlinearity):
-    r = linear_layer(input, output_dim)
-    r = nonlinearity(r)
-    return r
+    net = Convolution((5, 5), 64, init=cntk.initializer.glorot_uniform(scale=0.2), activation=cntk.ops.relu, pad=True)(net)
+    net = MaxPooling((3, 3), strides=(2, 2))(net)
 
-def fully_connected_classifier_net(input, num_output_classes, hidden_layer_dim, num_hidden_layers, nonlinearity):
-    h = dense_layer(input, hidden_layer_dim, nonlinearity)
-    for i in range(1, num_hidden_layers):
-        h = dense_layer(h, hidden_layer_dim, nonlinearity)
-    r = linear_layer(h, num_output_classes)
-    return r
+    net = Dense(64, init=cntk.initializer.glorot_uniform(scale=1.697))(net)
+    net = Dropout(dropout)(net)
+    net = Dense(out_dims, init=cntk.initializer.glorot_uniform(scale=0.212), activation=None)(net)
+
+    return net
+
 
 # Create the fully connected classfier but first we scale the input to 0-1 range by dividing each pixel by 256.
-scaled_input = element_times(constant(0.00390625), input)
-y = fully_connected_classifier_net(scaled_input, output_dim, hidden_layers_dim, num_hidden_layers, relu)
+scaled_input = cntk.ops.element_times(cntk.ops.constant(0.00390625), input)
+y = create_basic_model(scaled_input, output_dim, dropout=0.5)
 
 loss = cntk.ops.cross_entropy_with_softmax(y, label)
 label_error = cntk.ops.classification_error(y, label)
 
-# Instantiate the trainer object to drive the model training
-learning_rate_per_sample = 0.003125
-learner = cntk.sgd(y.parameters, lr=learning_rate_per_sample)
+#Initialize the parameters for the trainer
+train_epoch_size = 20000
+train_minibatch_size = 64
+train_max_epochs = 1
+
+# For basic model
+lr_per_sample = [0.00015625]*10+[0.000046875]*10+[0.0000156]
+momentum_per_sample = 0
+l2_reg_weight = 0.03
+
+# trainer ob# Instantiate the trainer object to drive the model training
+lr_schedule = cntk.learning_rate_schedule(lr_per_sample, units=train_epoch_size)
+learner = cntk.momentum_sgd(y.parameters,  lr_schedule, momentum_per_sample, l2_regularization_weight = l2_reg_weight)
 trainer = cntk.Trainer(y, loss, label_error, [learner])
 
-#Initialize the parameters for the trainer
-minibatch_size = 64
-num_samples_per_sweep = 60000
-num_sweeps_to_train_with = 10
-num_minibatches_to_train = (num_samples_per_sweep * num_sweeps_to_train_with) / minibatch_size
-
 # Run the trainer on and perform model training
-training_progress_output_freq = 500
+training_progress_output_freq = 10
 
-for i in range(0, int(num_minibatches_to_train)):
-    mb = train_mb_source.next_minibatch(minibatch_size)
+# loop over epochs
+for epoch in range(train_max_epochs):
+    sample_count = 0
+    num_minibatch = 0
+    # loop over minibatches in the epoch
+    while sample_count < train_epoch_size:
 
-    # Specify the mapping of input variables in the model to actual minibatch data to be trained with
-    arguments = {input: mb[features_train],
-                 label: mb[labels_train]}
-    trainer.train_minibatch(arguments)
+        minibatch = train_mb_source.next_minibatch(min(train_minibatch_size, train_epoch_size - sample_count))
 
-    # Print the training progress data
-    if i % training_progress_output_freq == 0:
-        training_loss = cntk.get_train_loss(trainer)
-        eval_error = cntk.get_train_eval_criterion(trainer)
-        print("%d - Loss: %f   Error: %f" % (i, training_loss, eval_error))
+        # Specify the mapping of input variables in the model to actual minibatch data to be trained with
+        data = {input: minibatch[features_train],
+                label: minibatch[labels_train]}
+        trainer.train_minibatch(data)
 
-# Evaluate on test data
+        sample_count += data[label].num_samples
+        num_minibatch += 1
+
+        # Print the training progress data
+        if num_minibatch % training_progress_output_freq == 0:
+            training_loss = cntk.get_train_loss(trainer)
+            eval_error = cntk.get_train_eval_criterion(trainer)
+            print("# Samples: %d - Loss: %f   Error: %f" % (sample_count, training_loss, eval_error))
+
+print("Training Completed.")
+
 # Test data for trained model
+test_epoch_size = 10000
 test_minibatch_size = 512
-num_samples = 10000
-num_minibatches_to_test = num_samples / test_minibatch_size
+
+sample_count = 0
 test_result = 0.0
-for i in range(0, int(num_minibatches_to_test)):
-    mb = test_mb_source.next_minibatch(test_minibatch_size)
+while sample_count < test_epoch_size:
+
+    minibatch = test_mb_source.next_minibatch(min(test_minibatch_size, test_epoch_size - sample_count))
 
     # Specify the mapping of input variables in the model to actual
     # minibatch data to be tested with
-    arguments = {input: mb[features_test],
-                 label: mb[labels_test]}
-    eval_error = trainer.test_minibatch(arguments)
+    data = {input: minibatch[features_test],
+            label: minibatch[labels_test]}
+    eval_error = trainer.test_minibatch(data)
     test_result = test_result + eval_error
 
+    sample_count += data[label].num_samples
+
 # Average of evaluation errors of all test minibatches
-print("Average errors of all test minibaches: {0:.2f}%".format(test_result*100 / num_minibatches_to_test))
+print("Average errors of all test minibaches: {0:.2f}%".format(test_result*100 / test_epoch_size))
