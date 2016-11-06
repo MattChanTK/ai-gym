2a5f9c5ac7bd507bcd1a1ce61403acb22ba36cfa
Matthew Chan <matthew.tk.chan@gmail.com>
2016-11-06 02:28:11 -0800

    Working implementation of MNIST classification using CNTK.

--- MNIST/minist_softmax_cntk.py
+++ MNIST/minist_softmax_cntk.py
@@ -1,31 +1,47 @@
-# Import the relevant components
 import gzip
 import os
 import struct
 import numpy as np
-import cntk
-from cntk.layers import Convolution, MaxPooling, AveragePooling, Dropout, BatchNormalization, Dense
 
 try:
     from urllib.request import urlretrieve
 except ImportError:
     from urllib import urlretrieve
 
+import cntk
+from cntk.layers import Convolution, MaxPooling, Dropout, Dense
 
-# Functions to load MNIST images and unpack into train and test set.
-# - loadData reads image data and formats into a 28x28 long array
-# - loadLabels reads the corresponding labels data, 1 for each image
-# - load packs the downloaded image and labels data into a combined format to be read later by CNTK text reader
 
-def loadData(src, cimg):
-    local_path = os.path.join(os.getcwd(), "MNIST_data", src)
+'''
+-----------------------------------------------------
+Functions to load or download MNIST images and unpack
+into training and testing sets.
+-----------------------------------------------------
+'''
+# loading data from local path if possible. Otherwise download from online sources
+def load_or_download_mnist_files(filename, num_samples, local_data_dir):
+
+    if (local_data_dir):
+        local_path = os.path.join(local_data_dir, filename)
+    else:
+        local_path = os.path.join(os.getcwd(), filename)
+
     if os.path.exists(local_path):
         gzfname = local_path
     else:
-        src = "http://yann.lecun.com/exdb/mnist/" + src
-        print ('Downloading ' + src)
-        gzfname, h = urlretrieve(src, local_path)
-        print ('Done.')
+        local_data_dir = os.path.dirname(local_path)
+        if not os.path.exists(local_data_dir):
+            os.makedirs(local_data_dir)
+        filename = "http://yann.lecun.com/exdb/mnist/" + filename
+        print ("Downloading from" + filename, end=" ")
+        gzfname, h = urlretrieve(filename, local_path)
+        print ("[Done]")
+
+    return gzfname
+
+def get_mnist_data(filename, num_samples, local_data_dir):
+
+    gzfname = load_or_download_mnist_files(filename, num_samples, local_data_dir)
 
     with gzip.open(gzfname) as gz:
         n = struct.unpack('I', gz.read(4))
@@ -34,27 +50,21 @@ def loadData(src, cimg):
             raise Exception('Invalid file: unexpected magic number.')
         # Read number of entries.
         n = struct.unpack('>I', gz.read(4))[0]
-        if n != cimg:
-            raise Exception('Invalid file: expected {0} entries.'.format(cimg))
+        if n != num_samples:
+            raise Exception('Invalid file: expected {0} entries.'.format(num_samples))
         crow = struct.unpack('>I', gz.read(4))[0]
         ccol = struct.unpack('>I', gz.read(4))[0]
         if crow != 28 or ccol != 28:
             raise Exception('Invalid file: expected 28 rows/cols per image.')
         # Read data.
-        res = np.fromstring(gz.read(cimg * crow * ccol), dtype = np.uint8)
+        res = np.fromstring(gz.read(num_samples * crow * ccol), dtype = np.uint8)
 
-        return res.reshape((cimg, crow * ccol))
+        return res.reshape((num_samples, crow * ccol))
 
+# loading labels from local path if possible. Otherwise download from online sources
+def get_mnist_labels(filename, num_samples, local_data_dir):
 
-def loadLabels(src, cimg):
-    local_path = os.path.join(os.getcwd(), "MNIST_data", src)
-    if os.path.exists(local_path):
-        gzfname = local_path
-    else:
-        src = "http://yann.lecun.com/exdb/mnist/" + src
-        print ('Downloading ' + src)
-        gzfname, h = urlretrieve(src, local_path)
-        print ('Done.')
+    gzfname = load_or_download_mnist_files(filename, num_samples, local_data_dir)
 
     with gzip.open(gzfname) as gz:
         n = struct.unpack('I', gz.read(4))
@@ -63,28 +73,28 @@ def loadLabels(src, cimg):
             raise Exception('Invalid file: unexpected magic number.')
         # Read number of entries.
         n = struct.unpack('>I', gz.read(4))
-        if n[0] != cimg:
-            raise Exception('Invalid file: expected {0} rows.'.format(cimg))
+        if n[0] != num_samples:
+            raise Exception('Invalid file: expected {0} rows.'.format(num_samples))
         # Read labels.
-        res = np.fromstring(gz.read(cimg), dtype = np.uint8)
+        res = np.fromstring(gz.read(num_samples), dtype = np.uint8)
 
-        return res.reshape((cimg, 1))
+        return res.reshape((num_samples, 1))
 
-
-def try_download(dataSrc, labelsSrc, cimg):
-    data = loadData(dataSrc, cimg)
-    labels = loadLabels(labelsSrc, cimg)
+# loading mnist data and labels
+def load_mnist_data(data_filename, labels_filename, number_samples, local_data_dir=None):
+    data = get_mnist_data(data_filename, number_samples, local_data_dir)
+    labels = get_mnist_labels(labels_filename, number_samples, local_data_dir)
     return np.hstack((data, labels))
 
 # Save the data files into a format compatible with CNTK text reader
-def savetxt(filename, ndarray):
+def save_as_txt(filename, ndarray):
     dir = os.path.dirname(filename)
 
     if not os.path.exists(dir):
         os.makedirs(dir)
 
     if not os.path.isfile(filename):
-        print("Saving", filename)
+        print("Saving to ", filename, end=" ")
         with open(filename, 'w') as f:
             labels = list(map(' '.join, np.eye(10, dtype=np.uint).astype(str)))
             for row in ndarray:
@@ -96,46 +106,56 @@ def savetxt(filename, ndarray):
         print("File already exists", filename)
 
 
+'''
+--------------------------------------------------
+Retrieve and process the training and testing data
+--------------------------------------------------
+'''
 # Ensure we always get the same amount of randomness
 np.random.seed(0)
 
 # Define the data dimensions
-input_dim = 784
+input_dim = 28*28
 output_dim = 10
+
+num_train_samples = 60000
+num_test_samples = 10000
 
 # Loading the MNIST training and testing data
 training_data_path = os.path.join(os.getcwd(), "MNIST_data", "Train-28x28_cntk_text.txt")
 testing_data_path = os.path.join(os.getcwd(), "MNIST_data", "Test-28x28_cntk_text.txt")
 
+# Download the data if they don't already exist
 if not os.path.exists(training_data_path):
 
-    url_train_image = 'train-images-idx3-ubyte.gz'
-    url_train_labels = 'train-labels-idx1-ubyte.gz'
-    num_train_samples = 60000
+    url_train_image = "train-images-idx3-ubyte.gz"
+    url_train_labels = "train-labels-idx1-ubyte.gz"
 
-    print("Loading train data")
-    train = try_download(url_train_image, url_train_labels, num_train_samples)
+    print("Loading training data")
+    saved_data_dir = os.path.join(os.getcwd(), "MNIST_data")
+    train = load_mnist_data(url_train_image, url_train_labels, num_train_samples, local_data_dir=saved_data_dir)
 
-    print ('Writing train text file...')
-    savetxt(training_data_path, train)
-    print('Done')
+    print ("Writing training data text file...")
+    save_as_txt(training_data_path, train)
+    print("[Done]")
 
 if not os.path.exists(testing_data_path):
 
-    url_test_image = 't10k-images-idx3-ubyte.gz'
-    url_test_labels = 't10k-labels-idx1-ubyte.gz'
-    num_test_samples = 10000
+    url_test_image = "t10k-images-idx3-ubyte.gz"
+    url_test_labels = "t10k-labels-idx1-ubyte.gz"
 
-    print("Loading test data")
-    test = try_download(url_test_image, url_test_labels, num_test_samples)
+    print("Loading testing data")
+    saved_data_dir = os.path.join(os.getcwd(), "MNIST_data2")
+    test = load_mnist_data(url_test_image, url_test_labels, num_test_samples, saved_data_dir)
 
-    print ('Writing test text file...')
-    savetxt(testing_data_path, test)
-    print('Done')
+    print ("Writing testing data text file...")
+    save_as_txt(testing_data_path, test)
+    print("[Done]")
 
 feature_stream_name = 'features'
 labels_stream_name = 'labels'
 
+# Convert to CNTK MinibatchSource
 train_mb_source = cntk.text_format_minibatch_source(training_data_path, [
     cntk.StreamConfiguration(feature_stream_name, input_dim),
     cntk.StreamConfiguration(labels_stream_name, output_dim)])
@@ -153,58 +173,71 @@ labels_test = test_mb_source[labels_stream_name]
 print("Test data from file {0} successfully read".format(testing_data_path))
 
 
-
+'''
+---------------------------------------------
+Constructing the Convolutional Neural Network
+---------------------------------------------
+'''
 image_shape = (1, 28, 28)
 input = cntk.ops.input_variable(image_shape, np.float32)
 label = cntk.ops.input_variable(output_dim, np.float32)
 
+def create_convolutional_neural_network(input, out_dims, dropout_prob=0.0):
 
-def create_basic_model(input, out_dims, dropout=0.0):
-    net = Convolution((5, 5), 32, init=cntk.initializer.glorot_uniform(scale=0.1557 / 256), activation=cntk.ops.relu, pad=True)(input)
-    net = MaxPooling((3, 3), strides=(2, 2))(net)
+    convolutional_layer_1 = Convolution((5, 5), 32, activation=cntk.ops.relu, pad=True)(input)
+    pooling_layer_1 = MaxPooling((2, 2), strides=(2, 2))(convolutional_layer_1)
 
-    net = Convolution((5, 5), 32, init=cntk.initializer.glorot_uniform(scale=0.2), activation=cntk.ops.relu, pad=True)(net)
-    net = MaxPooling((3, 3), strides=(2, 2))(net)
+    convolutional_layer_2 = Convolution((5, 5), 64, activation=cntk.ops.relu, pad=True)(pooling_layer_1)
+    pooling_layer_2 = MaxPooling((2, 2), strides=(2, 2))(convolutional_layer_2)
 
-    net = Convolution((5, 5), 64, init=cntk.initializer.glorot_uniform(scale=0.2), activation=cntk.ops.relu, pad=True)(net)
-    net = MaxPooling((3, 3), strides=(2, 2))(net)
+    fully_connected_layer = Dense(1024)(pooling_layer_2)
+    dropout_layer = Dropout(dropout_prob)(fully_connected_layer)
+    output_layer = Dense(out_dims, activation=None)(dropout_layer)
 
-    net = Dense(64, init=cntk.initializer.glorot_uniform(scale=1.697))(net)
-    net = Dropout(dropout)(net)
-    net = Dense(out_dims, init=cntk.initializer.glorot_uniform(scale=0.212), activation=None)(net)
+    return output_layer
 
-    return net
+# Scale the input to 0-1 range by dividing each pixel by 256
+scaled_input = cntk.ops.element_times(cntk.ops.constant(float(1/256)), input)
+
+# Create the convolutional neural network
+output = create_convolutional_neural_network(scaled_input, output_dim, dropout_prob=0.5)
 
 
-# Create the fully connected classfier but first we scale the input to 0-1 range by dividing each pixel by 256.
-scaled_input = cntk.ops.element_times(cntk.ops.constant(0.00390625), input)
-y = create_basic_model(scaled_input, output_dim, dropout=0.5)
-
-loss = cntk.ops.cross_entropy_with_softmax(y, label)
-label_error = cntk.ops.classification_error(y, label)
-
+'''
+----------------------
+Setting up the trainer
+----------------------
+'''
 #Initialize the parameters for the trainer
-train_epoch_size = 20000
 train_minibatch_size = 64
+learning_rate = 1e-4
+momentum = 0.9 ** (1.0 / train_minibatch_size)
+
+# Define the loss function
+loss = cntk.ops.cross_entropy_with_softmax(output, label)
+
+# Define the function that calculates classification error
+label_error = cntk.ops.classification_error(output, label)
+
+# Instantiate the trainer object to drive the model training
+learner = cntk.adam_sgd(output.parameters, learning_rate, momentum)
+trainer = cntk.Trainer(output, loss, label_error, [learner])
+
+
+'''
+-----------------------------------------
+Training the Convolutional Neural Network
+-----------------------------------------
+'''
+train_epoch_size = num_train_samples
 train_max_epochs = 1
-
-# For basic model
-lr_per_sample = [0.00015625]*10+[0.000046875]*10+[0.0000156]
-momentum_per_sample = 0
-l2_reg_weight = 0.03
-
-# trainer ob# Instantiate the trainer object to drive the model training
-lr_schedule = cntk.learning_rate_schedule(lr_per_sample, units=train_epoch_size)
-learner = cntk.momentum_sgd(y.parameters,  lr_schedule, momentum_per_sample, l2_regularization_weight = l2_reg_weight)
-trainer = cntk.Trainer(y, loss, label_error, [learner])
-
-# Run the trainer on and perform model training
 training_progress_output_freq = 10
 
 # loop over epochs
 for epoch in range(train_max_epochs):
     sample_count = 0
     num_minibatch = 0
+
     # loop over minibatches in the epoch
     while sample_count < train_epoch_size:
 
@@ -226,8 +259,13 @@ for epoch in range(train_max_epochs):
 
 print("Training Completed.")
 
-# Test data for trained model
-test_epoch_size = 10000
+
+'''
+-------------------
+Classification Test
+--------------------
+'''
+test_epoch_size = num_test_samples
 test_minibatch_size = 512
 
 sample_count = 0
@@ -236,8 +274,7 @@ while sample_count < test_epoch_size:
 
     minibatch = test_mb_source.next_minibatch(min(test_minibatch_size, test_epoch_size - sample_count))
 
-    # Specify the mapping of input variables in the model to actual
-    # minibatch data to be tested with
+    # Specify the mapping of input variables in the model to actual minibatch data to be tested with
     data = {input: minibatch[features_test],
             label: minibatch[labels_test]}
     eval_error = trainer.test_minibatch(data)
@@ -245,5 +282,5 @@ while sample_count < test_epoch_size:
 
     sample_count += data[label].num_samples
 
-# Average of evaluation errors of all test minibatches
-print("Average errors of all test minibaches: {0:.2f}%".format(test_result*100 / test_epoch_size))
+# Printing the average of evaluation errors of all test minibatches
+print("\nAverage errors of all test minibaches: {0:.6f}%".format(test_result*100 / test_epoch_size))
